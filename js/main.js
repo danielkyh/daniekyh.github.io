652ed8bf39da18e705e5d8de97a119e8ede28b2d
Daniel Huang <danielh714@gmail.com>
2016-02-13 13:56:19 -0800

    add text rotator library and add custom color rotation

--- js/main.js
+++ js/main.js
@@ -21,4 +21,10 @@ $(document).ready(function(){
 		$(this).css({"color": titleColor})
 	})
 
+	$(".rotate").textrotator({
+	  animation: "flipUp", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+	  separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+	  speed: 2000 // How many milliseconds until the next word show.
+	});
+
 })
